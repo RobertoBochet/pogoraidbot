0e2ad57a7bd759afd805efca90e2774df9e9352d
Roberto Bochet <robertobochet@gmail.com>
2019-06-07 12:57:11 +0200

    Remove useless property

--- service/app/raid.py
+++ service/app/raid.py
@@ -10,31 +10,22 @@ class Raid:
         default_factory=lambda: "".join(random.choice(string.ascii_letters + string.digits) for i in range(8)))
     gym_name: str = None
     level: int = None
+    is_hatched: bool = False
     end: any = None
     hatching: any = None
     hangout: any = None
     boss: str = None
-    aprx_time: bool = False
+    is_aprx_time: bool = False
     participants: Dict = field(default_factory=lambda: {})
 
-    @property
-    def is_hatched(self) -> bool:
-        return self.hatching is None
-
-    @property
-    def is_egg(self) -> bool:
-        return self.hatching is not None
-
-    def add_participant(self, id: int, name: str):
-        print(self.participants)
+    def add_participant(self, id: int, name: str) -> None:
         if id in self.participants:
-            print("aaa")
             v = self.participants[id]
             self.participants[id] = (name, "l", v[2] + 1)
         else:
             self.participants[id] = (name, "l", 1)
 
-    def add_flyer(self, id: int, name: str):
+    def add_flyer(self, id: int, name: str) -> None:
         if id in self.participants:
             v = self.participants[id]
             self.participants[id] = (name, "f", v[2] + 1)
@@ -53,30 +44,24 @@ class Raid:
 
     def to_msg(self) -> str:
 
-        level = ("\U00002B50" * self.level)
-
-        hatching = self.hatching.strftime("%H:%M")
-        end = self.end.strftime("%H:%M")
-
-        if self.aprx_time:
-            hatching = "~" + hatching
-            end = "~" + end
-
         msg = []
 
         if self.is_hatched:
             msg.append("<b>Boss</b>")
 
-        msg.append("<code>{}</code>".format(level))
+        msg.append("<code>{}</code>".format("\U00002B50" * self.level))
         msg.append("<i>{}</i>".format(self.gym_name))
 
-        if self.is_egg:
-            msg.append("<pre>Hatching:      {}</pre>".format(hatching))
+        if self.hatching is not None:
+            msg.append("<pre>Hatching:  {}{}</pre>"
+                       .format("~" if self.is_aprx_time else " ", self.hatching.strftime("%H:%M")))
 
-        msg.append("<pre>End:          {}</pre>".format(end))
+        if self.end is not None:
+            msg.append("<pre>End:       {}{}</pre>"
+                       .format("~" if self.is_aprx_time else " ", self.end.strftime("%H:%M")))
 
         if self.hangout is not None:
-            msg.append("<pre>Hangout:          {}</pre>".format(self.hangout.strftime("%H:%M")))
+            msg.append("<pre>Hangout:    {}</pre>".format(self.hangout.strftime("%H:%M")))
 
         msg.append("<code>[{}]</code>".format(self.code))
 
