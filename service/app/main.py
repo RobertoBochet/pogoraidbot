514ccadbaa63561c69537b39cdb098d78cf70cb9
Roberto Bochet <robertobochet@gmail.com>
2019-06-07 12:56:02 +0200

    Disabled debug for the bot library

--- service/app/main.py
+++ service/app/main.py
@@ -5,7 +5,11 @@ import os
 from bot import PoGORaidBot
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG,format="%(levelname)s|%(name)s|%(message)s")
+
+    # Set warning logging level for the module
+    logging.getLogger("telegram").setLevel(logging.WARNING)
+    logging.getLogger("JobQueue").setLevel(logging.WARNING)
 
     env = {"token": "BOT_TOKEN", "host": "REDIS_HOST", "port": "REDIS_PORT"}
     env = {k: os.environ[env[k]] for k in env if env[k] in os.environ}
