1d85e8ce0df1ee4cab91e07ee184cea72097e51a
Roberto Bochet <robertobochet@gmail.com>
2019-07-09 21:38:55 +0200

    Added support to inline arguments

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import logging
 import os
+import argparse
 
 from bot import PoGORaidBot
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.WARNING,format="%(levelname)s|%(name)s|%(message)s")
+    logging.basicConfig(level=logging.WARNING, format="%(levelname)s|%(name)s|%(message)s")
 
     # Set warning logging level for the module
     logging.getLogger("bot").setLevel(logging.DEBUG)
@@ -13,9 +14,33 @@ if __name__ == "__main__":
     logging.getLogger("telegram").setLevel(logging.WARNING)
     logging.getLogger("JobQueue").setLevel(logging.WARNING)
 
-    env = {"token": "BOT_TOKEN", "host": "REDIS_HOST", "port": "REDIS_PORT", "debug_folder": "DEBUG_FOLDER"}
-    env = {k: os.environ[env[k]] for k in env if env[k] in os.environ}
+    # Get environment variables
+    env = {}
 
-    bot = PoGORaidBot(**env)
+    env["token"] = os.getenv("BOT_TOKEN")
+    env["host"] = os.getenv("REDIS_HOST")
+    env["port"] = os.getenv("REDIS_PORT")
+    env["debug_folder"] = os.getenv("DEBUG_FOLDER")
 
+    # Get inline arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-t", "--token", dest="token", help="telegram bot token")
+    parser.add_argument("-r", "--host", dest="host", help="redis host")
+    parser.add_argument("-p", "--port", dest="port", help="redis port")
+    parser.add_argument("-d", "--debug-folder", dest="debug_folder", help="debug folder")
+
+    args = vars(parser.parse_args())
+
+    # Remove None elements
+    env = {k: env[k] for k in env if env[k] is not None}
+    args = {k: args[k] for k in args if args[k] is not None}
+
+    # Merge environment variables and inline arguments
+    args = {**env, **args}
+
+    # Create the bot
+    bot = PoGORaidBot(**args)
+
+    # Manage updates
     bot.listen()
