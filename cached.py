02d4e9bac5f3c69f4e191cf2a7dffb9c5c0dd787
Roberto Bochet <robertobochet@gmail.com>
2019-07-24 02:03:24 +0200

    Minor change

--- cached.py
+++ cached.py
@@ -1,16 +1,18 @@
 import functools
+from typing import Callable
 
 
 class Cached:
-    def __init__(self, func):
+    def __init__(self, func: Callable):
         self.func = func
         self.cached = False
+        self.cache = None
 
     def __get__(self, obj, objtype):
         """Support instance methods."""
         return functools.partial(self.__call__, obj)
 
-    def __call__(self, *args, **kwargs) -> bool:
+    def __call__(self, *args, **kwargs):
         if self.cached:
             return self.cache
 
