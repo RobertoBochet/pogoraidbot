3b11ea4d7eead299f2e6805a9b0db6f42a064ff3
Roberto Bochet <robertobochet@gmail.com>
2019-12-23 00:11:55 +0100

    Envs for data expiration were added

--- pogoraidbot/main_docker.py
+++ pogoraidbot/main_docker.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     env["redis"] = os.getenv("PGRB_REDIS")
     env["superadmin"] = os.getenv("PGRB_BOT_SUPERADMIN")
     env["gyms_file"] = os.getenv("PGRB_BOT_GYMS_FILE")
+    env["gyms_expiration"] = os.getenv("PGRB_BOT_GYMS_EXPIRATION")
     env["bosses_file"] = os.getenv("PGRB_BOT_BOSSES_FILE")
+    env["bosses_expiration"] = os.getenv("PGRB_BOT_BOSSES_EXPIRATION")
     env["log_level"] = os.getenv("PGRB_BOT_LOG_LEVEL")
 
     if os.getenv("PGRB_BOT_DEBUG_PATH") is not None:
